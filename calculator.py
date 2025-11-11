d0cdadb9224d0d9b4a258608c8efa6d3303a5d8e
skylar2077 <115901007+skylar2077@users.noreply.github.com>
2025-11-11 13:22:14 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -32,33 +32,3 @@ def log(a, b):
     raise ValueError
 def exp(a, b):
     return a ** b
-
-# print(log(2,6))
-
-
-
-
-# First example
-def add(a, b): 
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    else:
-        return b / a
-    
-def log(a, b):
-    if a <= 0:
-        raise ValueError
-    else:
-        return math.log(b, a)
-    
-def exp(a, b):
-    return math.pow(a, b)
