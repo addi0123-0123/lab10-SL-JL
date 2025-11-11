2db49a63bb36d00de8745cbef1a7a0d0fe863f0b
addi0123-0123 <addi0123@pm.me>
2025-11-11 13:05:32 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,27 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a*b
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a/b
+def log(a, b):
+    if b > 0 and b != 1 and a > 0:
+        return math.log(b,a)
+    raise ValueError
+def exp(a, b):
+    return a ** b
+
+print(log(2,6))
+
+
 
 
